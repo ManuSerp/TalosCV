925a8b2e060894d79f79a74aecc74dadde830193
Serp <manu.serpette74@gmail.com>
2021-11-05 08:40:43 +0100

    ROSconfig

---
+++ ROSconfig/config.sh
@@ -0,0 +1,6 @@
+echo '------------ROS------------'
+echo 'ENVIRONEMENT SETUP'
+echo 'Adding ROS to PATH var'
+source /opt/ros/noetic/setup.bash
+printenv | grep ROS
+echo '--------------------------'
