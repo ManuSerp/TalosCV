09af9662e29d9a46fed3caf79e4a96ab11c62546
Serp <manu.serpette74@gmail.com>
2022-01-03 11:07:05 +0100

    comments

--- ROS/catkin_ws/src/tracker_cam/scripts/cam/coordKCC.py
+++ ROS/catkin_ws/src/tracker_cam/scripts/cam/coordKCC.py
@@ -13,4 +13,4 @@ def mapper(d,max=4):
 
 def spatialization(centre,depth,cam_angle_hz=57,cam_angle_vt=57,fen_size=(640,480)):
     angl=[(centre[0]*2*cam_angle_hz/fen_size[0])-cam_angle_hz,-(centre[1]*2*cam_angle_vt/fen_size[1])+cam_angle_vt] 
-    return [depth,tan(angl[1]*3.1418/180)*depth,tan(angl[0]*3.1418/180)*depth]
+    return [depth,tan(angl[1]*3.1418/180)*depth,tan(angl[0]*3.1418/180)*depth] #x,y,z dans gazebo
