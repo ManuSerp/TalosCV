c370c14f61139c2d6d0faa0c592849c8c6f91d0f
Serp <manu.serpette74@gmail.com>
2022-01-03 11:06:31 +0100

    spatialization

--- ROS/catkin_ws/src/tracker_cam/scripts/cam/coordKCC.py
+++ ROS/catkin_ws/src/tracker_cam/scripts/cam/coordKCC.py
@@ -1,3 +1,5 @@
+from math import tan
+
 def center(bbox):
     return [bbox[0]+bbox[2]//2,bbox[1]+bbox[3]//2]    
 
@@ -11,4 +13,4 @@ def mapper(d,max=4):
 
 def spatialization(centre,depth,cam_angle_hz=57,cam_angle_vt=57,fen_size=(640,480)):
     angl=[(centre[0]*2*cam_angle_hz/fen_size[0])-cam_angle_hz,-(centre[1]*2*cam_angle_vt/fen_size[1])+cam_angle_vt] 
-    
+    return [depth,tan(angl[1]*3.1418/180)*depth,tan(angl[0]*3.1418/180)*depth]
