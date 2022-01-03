5433c1b2bc7328356c094b7eb83b3b86dd559248
Serp <manu.serpette74@gmail.com>
2022-01-03 10:52:19 +0100

    sapatilization

--- ROS/catkin_ws/src/tracker_cam/scripts/cam/coordKCC.py
+++ ROS/catkin_ws/src/tracker_cam/scripts/cam/coordKCC.py
@@ -6,6 +6,9 @@ def angleCenter(bbox,cam_angle_hz=57,cam_angle_vt=57,fen_size=(640,480)):
     centre=center(bbox)
     return [(centre[0]*2*cam_angle_hz/fen_size[0])-cam_angle_hz,-(centre[1]*2*cam_angle_vt/fen_size[1])+cam_angle_vt,centre]
 
-
 def mapper(d,max=4):
     return 0
+
+def spatialization(centre,depth,cam_angle_hz=57,cam_angle_vt=57,fen_size=(640,480)):
+    angl=[(centre[0]*2*cam_angle_hz/fen_size[0])-cam_angle_hz,-(centre[1]*2*cam_angle_vt/fen_size[1])+cam_angle_vt] 
+    
