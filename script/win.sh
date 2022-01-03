959f40d4b4f57ddbb743dd2fb691fe2240da2f17
Serp <manu.serpette74@gmail.com>
2022-01-03 15:10:48 +0100

    test multi term

---
+++ script/win.sh
@@ -0,0 +1,5 @@
+
+
+export PATH=$PATH:/opt/ros/noetic/bin:/home/manu/anaconda3/bin:/home/manu/anaconda3/condabin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/usr/local/go/bin
+
+roscore
