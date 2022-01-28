6ad470707755f5dd4a207382d5f752629832c498
Serp <manu.serpette74@gmail.com>
2022-01-28 14:09:27 +0100

    autopep8

--- ROS/catkin_ws/src/tracker_cam/scripts/tracking.py
+++ ROS/catkin_ws/src/tracker_cam/scripts/tracking.py
@@ -17,10 +17,7 @@ from pysot.models.model_builder import ModelBuilder
 from pysot.tracker.tracker_builder import build_tracker
 
 
-
-
 from cam.coordKCC import angleCenter
-
 
 
 torch.set_num_threads(1)
@@ -31,8 +28,6 @@ parser.add_argument('--snapshot', type=str, help='model name')
 parser.add_argument('--video_name', default='', type=str,
                     help='videos or image files')
 args = parser.parse_args()
-
-
 
 
 def get_frames(video_name):
@@ -48,12 +43,12 @@ def get_frames(video_name):
             else:
                 break
     elif video_name.endswith('avi') or \
-        video_name.endswith('mp4'):
+            video_name.endswith('mp4'):
         cap = cv2.VideoCapture(args.video_name)
         while True:
             ret, frame = cap.read()
             if ret:
-                yield    frame
+                yield frame
             else:
                 break
     else:
@@ -66,7 +61,7 @@ def get_frames(video_name):
 
 
 def track():
-    cptt=0
+    cptt = 0
     flt = 1
     # load config
     cfg.merge_from_file(args.config)
@@ -79,7 +74,7 @@ def track():
 
     # load model
     model.load_state_dict(torch.load(args.snapshot,
-        map_location=lambda storage, loc: storage.cpu()))
+                                     map_location=lambda storage, loc: storage.cpu()))
     model.eval().to(device)
 
     # build tracker
@@ -92,9 +87,9 @@ def track():
         video_name = 'webcam'
     cv2.namedWindow(video_name, cv2.WND_PROP_FULLSCREEN)
     for frame in get_frames(args.video_name):
-        if flt==cptt :
+        if flt == cptt:
 
-            s=time.time()
+            s = time.time()
             if first_frame:
                 try:
                     init_rect = cv2.selectROI(video_name, frame, False, False)
@@ -107,42 +102,41 @@ def track():
                 if 'polygon' in outputs:
 
                     polygon = np.array(outputs['polygon']).astype(np.int32)
-                
+
                     cv2.polylines(frame, [polygon.reshape((-1, 1, 2))],
-                              True, (0, 255, 0), 3)
+                                  True, (0, 255, 0), 3)
                     mask = ((outputs['mask'] > cfg.TRACK.MASK_THERSHOLD) * 255)
                     mask = mask.astype(np.uint8)
                     mask = np.stack([mask, mask*255, mask]).transpose(1, 2, 0)
                     frame = cv2.addWeighted(frame, 0.77, mask, 0.23, -1)
                 else:
                     bbox = list(map(int, outputs['bbox']))
-                
+
                     cv2.rectangle(frame, (bbox[0], bbox[1]),
                                   (bbox[0]+bbox[2], bbox[1]+bbox[3]),
                                   (0, 255, 0), 3)
 
-                #### affichage des angles
-                    angle=angleCenter(bbox)
-                    font = cv2.FONT_HERSHEY_SIMPLEX  
-                    cv2.putText(frame, 'HZ:' +str(int(angle[0]))+'deg VT:'+str(int(angle[1]))+'deg Dist: ??', (10, 40),  font, 1,    (0, 255, 0),                  
-                  2, 
-                 cv2.LINE_4)
+                # affichage des angles
+                    angle = angleCenter(bbox)
+                    font = cv2.FONT_HERSHEY_SIMPLEX
+                    cv2.putText(frame, 'HZ:' + str(int(angle[0]))+'deg VT:'+str(int(angle[1]))+'deg ', (10, 40),  font, 1,    (0, 255, 0),
+                                2,
+                                cv2.LINE_4)
 
-                 ### fin angles
-                e=time.time()
+                 # fin angles
+                e = time.time()
                 print(e-s)
                 cv2.imshow(video_name, frame)
                 cv2.waitKey(40)
-                cptt=0
-                if flt ==1:
-                    flt=2
+                cptt = 0
+                if flt == 1:
+                    flt = 2
 
                 else:
-                    flt=1    
+                    flt = 1
         else:
-            cptt=cptt+1
+            cptt = cptt+1
+
 
 if __name__ == '__main__':
     track()
-
-
