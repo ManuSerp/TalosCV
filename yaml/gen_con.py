8fcf6fb59b4a7a2be966db7c57e4dbf45962a50b
Serp <manu.serpette74@gmail.com>
2022-04-25 18:54:40 +0200

    gen done

--- yaml/gen_con.py
+++ yaml/gen_con.py
@@ -19,13 +19,33 @@ f.write("\tpos: [0.0, 0.0, -0.4]\n")
 f.write("v_head: \n")
 f.write("\tref: \"head_2_link\"\n")
 f.write("\tpos: [0.04, 0.05, 0.0]\n")
-# fin du frames.yml
-for i in range(int(args.lenght/0.05)):
+# fin du header frames.yml
+for i in range(int((args.lenght-0.15)/0.05)):
     f.write("v_field"+str(i+1)+": \n")
     f.write("\tref: \"head_2_link\"\n")
-    f.write("\tpos: ["+str(0.2+i*0.05)+", 0.0, 0.0]\n")
+    f.write("\tpos: [%.2f, 0.0, 0.0]\n" % (0.2+i*0.05))
+# fin du frames.yml
+f.close()
+f = open("gen/tasks.yml", "w")
 
+
+# debut de tasks.yml
+f.write("sc-elbow:\n")
+f.write("\ttype: self-collision\n")
+f.write("\ttracked: arm_4_link\n")
+f.write("\tradius: 0.1\n")
+f.write("\tavoided:\n")
+f.write("\t\tbase_link: 0.330\n")
+f.write("\t\ttorso_lift_link: 0.25\n")
+f.write("\t\tv_torso_1: 0.2\n")
+f.write("\t\tv_torso_2: 0.2\n")
+f.write("\t\tv_head: 0.15\n")
+# fin header
+for i in range(int((args.lenght-0.15)/0.05)):
+    a = "%.2f" % (0.1+i*0.02)
+    f.write("\t\tv_field"+str(i+1)+": "+a+"\n")
+# fin loop
+f.write("\tweight: 1000\n\tkp: 50.0\n\tkd: 500.0\n\tm: 0.2\n\tmargin: 0.02\n")
 
 f.close()
-
 print("python done...")
